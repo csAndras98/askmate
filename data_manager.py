00e623c2077cf2c1c00220bac3259845193a09f7
romanbiankaeszter <55354913+romanbiankaeszter@users.noreply.github.com>
2020-01-21 14:18:37 +0100

    test sql

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,13 @@
 import connection
 
-question = "./static/question.csv"
-answer = "./static/answer.csv"
+@connection.connection_handler
+def get_all_questions(cursor):
+    cursor.execute("""Select title from question""")
+    questions = cursor.fetchall()
+    return questions
 
 
-def question_list():
+"""def question_list():
     table = connection.get_table_from_file(question)
     table.remove(table[0])
     table.reverse()
@@ -35,4 +38,4 @@ def save_answer(saved_answer,q_id):
         write.append("WOP")
     write.append(q_id)
     write.append(saved_answer[0])
-    connection.write_table_to_file(answer, write)
+    connection.write_table_to_file(answer, write)"""
