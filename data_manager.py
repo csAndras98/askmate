bb596ec0c5fd28075bd9c1c4e59e337795bb644a
tompuljalmeg <55482889+tompuljalmeg@users.noreply.github.com>
2020-02-04 10:14:24 +0100

    Add files via upload

--- data_manager.py
+++ data_manager.py
@@ -26,20 +26,16 @@ def title_order_by_desc(cursor):
 
 @connection.connection_handler
 def get_answers(cursor):
-    cursor.execute("""Select id, message, submission_time, vote_number, question_id from answer """)
+    cursor.execute("""Select id, message, submission_time, vote_number, question_id from answer
+    order by submission_time desc """)
     answer = cursor.fetchall()
     return answer
 
-@connection.connection_handler
-def add_question(cursor,new_question,file_path):
-    cursor.execute("""INSERT INTO  question(title, message) VALUES """,{'title':new_question['title']}, {'message':new_question['message'], 'file_path': file_path})
-    new_question = cursor.fetchall()
-    return new_question
 
 @connection.connection_handler
 def save_question(cursor, title, message):
-    cursor.execute("""INSERT INTO question (submission_time, vote_number, title, message) 
-    VALUES (%s, 0, %s, %s)""",
+    cursor.execute("""INSERT INTO question (submission_time, view_number, vote_number, title, message) 
+    VALUES (%s, 0, 0, %s, %s)""",
                    [datetime.datetime.now(), title, message])
 
 @connection.connection_handler
